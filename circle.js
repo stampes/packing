cd5f1fb27150fac5cdea1492d47a82a55c4b7373
lsv <lsv@simcorp.com>
2020-10-21 16:26:17 +0200

    Formalized images

--- circle.js
+++ circle.js
@@ -5,20 +5,25 @@ function Circle(x, y, color) {
   this.color = color;
   this.growing = true;
 
-  this.grow = function() {
+  this.grow = function () {
     if (this.growing) {
       this.r += growthSpeed;
     }
-  }
+  };
 
-  this.show = function() {
+  this.show = function () {
     noStroke();
     fill(this.color);
-   // https://stackoverflow.com/questions/60179313/how-to-fill-p5-js-shape-with-an-image
+    // https://stackoverflow.com/questions/60179313/how-to-fill-p5-js-shape-with-an-image
     ellipse(this.x, this.y, this.r * 2, this.r * 2);
-  }
+  };
 
-  this.edges = function() {
+  this.edges = function () {
     return (this.x + this.r >= width || this.x - this.r <= 0 || this.y + this.r >= height || this.y - this.r <= 0)
-  }
+  };
+
+  this.overlap = function (x, y) {
+    let d = dist(x, y, this.x, this.y);
+    return (d - 2 < this.r);
+  };
 }
--- sketch.js
+++ sketch.js
@@ -1,45 +1,78 @@
-var circles;
-var img;
-var pixelsPerFrame = 15;
-var growthSpeed = 0.5;
+let circles;
+let img;
+let pixelsPerFrame = 100;
+let growthSpeed = 2.5;
+let maxLoops = 3000;
+let tiles = [];
+let tilesDb;
+
+function extractImg(src) {
+  return src.map(element => {
+    let ref = element.app_img.substring(10);
+    let last = ref.indexOf('"');
+
+    return ref.substring(0, last);
+  });
+}
+
+function storeTile(tileimg, error) {
+  if (!error) {
+    tiles.push(tileimg);
+  }
+}
+
+
+function loadTiles(src) {
+  let imgs = extractImg(src);
+  ims = [...new Set(imgs)];
+  imgs.map(e => loadImage(e, storeTile));
+}
 
 function preload() {
+
   img = loadImage("img.jpg");
 }
 
+
 function setup() {
   createCanvas(img.width, img.height);
-  var density = displayDensity();
+  let density = displayDensity();
   //pixelDensity(1);
   img.loadPixels();
   circles = [];
+  tilesDb = loadJSON("mapping.json", loadTiles);
 }
 
 function draw() {
   background(0);
 
-  var total = pixelsPerFrame;
-  var count = 0;
-  var attempts = 0;
+  let total = pixelsPerFrame;
+  let count = 0;
+  let attempts = 0;
 
   while (count < total) {
-    var newC = newCircle();
+    let newC = newCircle();
     if (newC !== null) {
       circles.push(newC);
       count++;
     }
     attempts++;
-    if (attempts > 3000) {
+    if (attempts > maxLoops) {
       noLoop();
       console.log("finished");
       let minr = Infinity;
       let maxr = 0;
       let tilesize = 7.5;
       let tiles = 0;
-      for (var i = 0; i < circles.length; i++) {
-        if (circles[i].r > maxr) { maxr = circles[i].r; }
-        else if (circles[i].r < minr) { minr = circles[i].r; }
-        if (tilesize<circles[i].r){tiles+=1;}
+      for (let i = 0; i < circles.length; i++) {
+        if (circles[i].r > maxr) {
+          maxr = circles[i].r;
+        } else if (circles[i].r < minr) {
+          minr = circles[i].r;
+        }
+        if (tilesize < circles[i].r) {
+          tiles += 1;
+        }
       }
       console.log(maxr);
       console.log(minr);
@@ -49,21 +82,22 @@ function draw() {
     }
   }
 
-  for (var i = 0; i < circles.length; i++) {
-    var circle = circles[i];
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
 
     if (circle.growing) {
       if (circle.edges()) {
         circle.growing = false;
       } else {
-        for (var j = 0; j < circles.length; j++) {
-          var other = circles[j];
+        for (let j = 0; j < circles.length; j++) {
+          let other = circles[j];
           if (circle !== other) {
-            var d = dist(circle.x, circle.y, other.x, other.y);
-            var distance = circle.r + other.r;
+            let d = dist(circle.x, circle.y, other.x, other.y);
+            let distance = circle.r + other.r;
 
             if (d - 1 < distance) {
               circle.growing = false;
+              //other.growing = false;
               break;
             }
           }
@@ -77,24 +111,23 @@ function draw() {
 }
 
 function newCircle() {
-  var x = random(0, img.width);
-  var y = random(0, img.height);
+  let x = random(0, img.width);
+  let y = random(0, img.height);
 
-  var valid = true;
-  for (var i = 0; i < circles.length; i++) {
-    var circle = circles[i];
-    var d = dist(x, y, circle.x, circle.y);
-    if (d - 2 < circle.r) {
+  let valid = true;
+  for (let i = 0; i < circles.length; i++) {
+    let circle = circles[i];
+    if (circle.overlap(x, y)) {
       valid = false;
       break;
     }
   }
   if (valid) {
-    var index = (int(x) + int(y) * img.width) * 4;
-    var r = img.pixels[index];
-    var g = img.pixels[index + 1];
-    var b = img.pixels[index + 2];
-    var c = color(r, g, b);
+    let index = (int(x) + int(y) * img.width) * 4;
+    let r = img.pixels[index];
+    let g = img.pixels[index + 1];
+    let b = img.pixels[index + 2];
+    let c = color(r, g, b);
     return new Circle(x, y, color(c));
   } else {
     return null;
